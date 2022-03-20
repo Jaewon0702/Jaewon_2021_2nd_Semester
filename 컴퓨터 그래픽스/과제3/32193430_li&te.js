ec3a2bc5988885b5b777672cbbf10a4d950abea5
Jaewon Lee <67958329+Jaewon0702@users.noreply.github.com>
2022-03-20 21:27:47 +0900

    Add files via upload

---
+++ 컴퓨터 그래픽스/과제3/32193430_li&te.js
@@ -0,0 +1,332 @@
+"use strict";
+
+var canvas;
+var gl;
+
+var numTimesToSubdivide = 0;
+
+var index = 0;
+
+var pointsArray = [];
+var normalsArray = [];
+
+
+var near = -10;
+var far = 10;
+var radius = 1.5;
+var theta = 0.0;
+var phi    = 0.0;
+var dr = 5.0 * Math.PI/180.0;
+
+var near1 = 0.3;
+var far1 = 3.0;
+var radius1 = 4.0;
+
+
+var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
+var  aspect = 1.0;       // Viewport aspect ratio
+
+
+var left = -3.0;
+var right = 3.0;
+var ytop =3.0;
+var bottom = -3.0;
+
+var va = vec4(0.0, 0.0, -1.0,1);
+var vb = vec4(0.0, 0.942809, 0.333333, 1);
+var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
+var vd = vec4(0.816497, -0.471405, 0.333333,1);
+
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialShininess = 20.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var normalMatrix, normalMatrixLoc;
+
+var eye;
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+
+var c = 0, s = 0, o = 0;
+
+var texSize = 128;
+
+function configureTexture( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB,
+         gl.RGB, gl.UNSIGNED_BYTE, image );
+    gl.generateMipmap( gl.TEXTURE_2D );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+}
+
+var image1 = new Array()
+    for (var i =0; i<texSize; i++)  image1[i] = new Array();
+    for (var i =0; i<texSize; i++)
+        for ( var j = 0; j < texSize; j++)
+           image1[i][j] = new Float32Array(4);
+    for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
+        var c = (((i & 0x8) == 0) ^ ((j & 0x8)  == 0));
+        image1[i][j] = [c, c, c, 1];
+    }
+
+// Convert floats to ubytes for texture
+
+var image2 = new Uint8Array(4*texSize*texSize);
+
+    for ( var i = 0; i < texSize; i++ )
+        for ( var j = 0; j < texSize; j++ )
+           for(var k =0; k<4; k++)
+                image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
+
+
+function triangle(a, b, c) {
+
+     var t1 = subtract(b, a);
+     var t2 = subtract(c, a);
+     var normal = normalize(cross(t2, t1));
+     normal = vec4(normal);
+     normal[3]  = 0.0;
+
+     normalsArray.push(normal);
+     normalsArray.push(normal);
+     normalsArray.push(normal);
+
+
+     pointsArray.push(a);
+     pointsArray.push(b);
+     pointsArray.push(c);
+
+     index += 3;
+}
+
+
+function divideTriangle(a, b, c, count) {
+    if ( count > 0 ) {
+
+        var ab = mix( a, b, 0.5);
+        var ac = mix( a, c, 0.5);
+        var bc = mix( b, c, 0.5);
+
+        ab = normalize(ab, true);
+        ac = normalize(ac, true);
+        bc = normalize(bc, true);
+
+        divideTriangle( a, ab, ac, count - 1 );
+        divideTriangle( ab, b, bc, count - 1 );
+        divideTriangle( bc, c, ac, count - 1 );
+        divideTriangle( ab, bc, ac, count - 1 );
+    }
+    else {
+        triangle( a, b, c );
+    }
+}
+
+
+function tetrahedron(a, b, c, d, n) {
+    divideTriangle(a, b, c, n);
+    divideTriangle(d, c, b, n);
+    divideTriangle(a, d, b, n);
+    divideTriangle(a, c, d, n);
+}
+
+window.onload = function init() {
+
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+
+    aspect =  canvas.width/canvas.height;
+
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+
+    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
+
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal);
+
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    normalMatrixLoc = gl.getUniformLocation( program, "normalMatrix" );
+
+    document.getElementById("Button0").onclick = function(){radius *= 2.0; c = 1;};
+    document.getElementById("Button1").onclick = function(){radius *= 0.5; c = 1;};
+    document.getElementById("Button2").onclick = function(){theta += dr; c = 1;};
+    document.getElementById("Button3").onclick = function(){theta -= dr;c = 1;};
+    document.getElementById("Button4").onclick = function(){phi += dr; c = 1;};
+    document.getElementById("Button5").onclick = function(){phi -= dr; c = 1;};
+
+    document.getElementById("Button6").onclick = function(){
+        numTimesToSubdivide++;
+        index = 0;
+        pointsArray = [];
+        normalsArray = [];
+        init();
+        c = 1;
+    };
+    document.getElementById("Button7").onclick = function(){
+        if(numTimesToSubdivide) numTimesToSubdivide--;
+        index = 0;
+        pointsArray = [];
+        normalsArray = [];
+        init();
+        c = 1;
+    };
+
+    document.getElementById("Button8").onclick = function(){o = 1;} // texture 버튼 켜기
+
+    document.getElementById("zFarSlider").onchange = function(event) {
+        far1 = event.target.value;
+        c = 0;
+    };
+    document.getElementById("zNearSlider").onchange = function(event) {
+        near1 = event.target.value;
+        c = 0;
+    };
+    
+    document.getElementById("radiusSlider").onchange = function(event) {
+       radius1 = event.target.value;
+       c = 0;
+    };
+    document.getElementById("thetaSlider").onchange = function(event) {
+        theta = event.target.value* Math.PI/180.0;
+        c = 0;
+    };
+    document.getElementById("phiSlider").onchange = function(event) {
+        phi = event.target.value* Math.PI/180.0;
+        c = 0;
+    };
+    document.getElementById("aspectSlider").onchange = function(event) {
+        aspect = event.target.value;
+        c = 0;
+    };
+    document.getElementById("fovSlider").onchange = function(event) {
+        fovy = event.target.value;
+        c = 0;
+    };
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+    
+    
+    
+
+
+    gl.uniform4fv( gl.getUniformLocation(program,
+       "ambientProduct"),flatten(ambientProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program,
+       "diffuseProduct"),flatten(diffuseProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program,
+       "specularProduct"),flatten(specularProduct) );
+    gl.uniform4fv( gl.getUniformLocation(program,
+       "lightPosition"),flatten(lightPosition) );
+    gl.uniform1f( gl.getUniformLocation(program,
+       "shininess"),materialShininess );
+
+
+    var texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0,
+            gl.RGBA, gl.UNSIGNED_BYTE, image2);
+    gl.generateMipmap( gl.TEXTURE_2D );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                    gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0)
+
+  
+
+    render();
+}
+
+
+function render() {
+
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+
+    eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
+    radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+    modelViewMatrix = lookAt(eye, at , up);  
+
+    
+    if(c == 1){
+        projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+
+        
+       // c = 0;
+    }
+    else{
+        projectionMatrix = perspective(fovy, aspect, near1, far1);
+
+
+        //c = 1;
+    }
+ 
+    normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
+    
+
+    for( var i=0; i<index; i+=3)
+        gl.drawArrays( gl.TRIANGLES, i, 3 );
+
+  // requestAnimFrame(render);
+     window.requestAnimFrame(render);
+}
